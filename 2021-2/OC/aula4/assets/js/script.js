f2c2340979c3d7dec18b8de0a46328636095231d
João Lucas Pereira Rafael <61843626+joao-lusa@users.noreply.github.com>
2021-08-26 20:19:46 -0300

    melhorando

--- 2021-2/OC/aula4/assets/js/script.js
+++ 2021-2/OC/aula4/assets/js/script.js
@@ -25,3 +25,17 @@ function sobre(){
 function contact(){
     $('article').load('views/contact.html')
 }
+
+function loadData(){
+    var name = document.getElementById("contact_name").value
+    var email = document.getElementById("contact_email").value
+    var about = document.getElementById("contact_about").value
+    var phone = document.getElementById("contact_phone").value
+
+    $('article').load('views/person.html', function(){
+        document.getElementById("nameContact").innerHTML = name 
+        document.getElementById("emailContact").innerHTML = email
+        document.getElementById("aboutContact").innerHTML = about
+        document.getElementById("phoneContact").innerHTML = phone
+    })
+}
